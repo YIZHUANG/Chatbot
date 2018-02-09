3137c5c0a23e31106921dca7d4d5f9e29264ca06
YI ZHUANG <urlikeahurricane@gmail.com>
2018-02-10 00:02:23 +0200

    new

--- services/passport.js
+++ services/passport.js
@@ -27,7 +27,7 @@ module.exports = (app, passport) => {
       {
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: '/api/google/callback',
+        callbackURL: 'http://mysterious-eyrie-35636.herokuapp.com/api/google/callback',
         proxy: true
       },
       (accessToken, refreshToken, profile, done) => {
